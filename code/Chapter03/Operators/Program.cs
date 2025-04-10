20514598d9195e17ab9f6857c62383d712d8090a
CollaborationDigital <66643351+CollaborationDigital@users.noreply.github.com>
2025-04-10 14:45:34 +0100

    Update Program.cs
    To get the right formatting of the table

--- code/Chapter03/Operators/Program.cs
+++ code/Chapter03/Operators/Program.cs
@@ -29,19 +29,20 @@ WriteLine($"g / f = {g / f}");
 
 #region Exploring logical operators
 
-bool p = true;
-bool q = false;
-WriteLine($"AND  | p     | q    ");
-WriteLine($"p    | {p & p,-5} | {p & q,-5} ");
-WriteLine($"q    | {q & p,-5} | {q & q,-5} ");
-WriteLine();
-WriteLine($"OR   | p     | q    ");
-WriteLine($"p    | {p | p,-5} | {p | q,-5} ");
-WriteLine($"q    | {q | p,-5} | {q | q,-5} ");
-WriteLine();
-WriteLine($"XOR  | p     | q    ");
-WriteLine($"p    | {p ^ p,-5} | {p ^ q,-5} ");
-WriteLine($"q    | {q ^ p,-5} | {q ^ q,-5} ");
+                bool p = true; 
+                bool q = false; 
+
+                WriteLine($"{"AND",-5} | {"p",-5} | {"q",-5} "); 
+                WriteLine($"{"p",-5} | {p & p,-5} | {p & q,-5} "); 
+                WriteLine($"{"q",-5} | {q & p,-5} | {q & q,-5} "); 
+                WriteLine(); 
+                WriteLine($"{"OR",-5} | {"p",-5} | {"q",-5}"); 
+                WriteLine($"{"p",-5} | {p | p,-5} | {p | q,-5} "); 
+                WriteLine($"{"q",-5} | {q | p,-5} | {q | q,-5} "); 
+                WriteLine(); 
+                WriteLine($"{"XOR",-5} | {"p",-5} | {"q",-5}");
+                WriteLine($"{"p",-5} | {p ^ p,-5} | {p ^ q,-5} "); 
+                WriteLine($"{"q",-5} | {q ^ p,-5} | {q ^ q,-5} ");
 
 #endregion
 
